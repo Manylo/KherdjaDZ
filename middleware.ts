f863bd72c1cac4e5c41bb2acab1425583f96c281
Manylo <manylamarkhodja@outlook.fr>
2024-05-30 00:33:54 +0100

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,12 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware(
-    {
-        publicRoutes: [
-          '/',
-          '/events/:id',
-          '/api/webhook/clerk',
-          '/api/webhook/stripe',
-          '/api/uploadthing'
-        ],
-        ignoredRoutes: [
-          '/api/webhook/clerk',
-          '/api/webhook/stripe',
-          '/api/uploadthing'
-        ]
-      }
-);
+export default clerkMiddleware({
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+});
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
